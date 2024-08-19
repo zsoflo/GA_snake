ec9fa3a0139fe215e4e47d4577f1dc9f7ed77ffb
zsoflo <zsofia1417@gmail.com>
2024-08-19 13:21:01 -0500

    netnetnet

--- network.py
+++ network.py
@@ -1,6 +1,19 @@
 import random
-
 import numpy as np
+from typing import NewType, List
+
+# UserID = int
+# user = UserID(10)
+# print(user)
+
+UserID = NewType('UserID', int)
+user = UserID(10)
+
+def find_user(user_id: UserID):
+    print('Found:', user_id)
+
+find_user(10)
+find_user(10)
 
 def sigmoid(z):
     return 1.0/(1.0+np.exp(-z))
@@ -18,8 +31,3 @@ class Network(object):
         for b, w in zip(self.biases, self.weights):
             a = sigmoid(np.dot(w, a)+b)
         return a
-        
-
-
-
-
