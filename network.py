8f5ff66c3f6a3c08d3be11c8cba45639ff1cab31
zsoflo <zsofia1417@gmail.com>
2024-08-19 13:31:36 -0500

    removed type

--- network.py
+++ network.py
@@ -1,19 +1,5 @@
 import random
 import numpy as np
-from typing import NewType, List
-
-# UserID = int
-# user = UserID(10)
-# print(user)
-
-UserID = NewType('UserID', int)
-user = UserID(10)
-
-def find_user(user_id: UserID):
-    print('Found:', user_id)
-
-find_user(10)
-find_user(10)
 
 def sigmoid(z):
     return 1.0/(1.0+np.exp(-z))
