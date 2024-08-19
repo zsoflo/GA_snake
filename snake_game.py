0d3469ca4433d0e8d83ab729daf50d77900ddcd4
Jonathan <sc17j3a@leeds.ac.uk>
2024-08-19 19:39:10 +0100

    Fixing formatting

--- snake_game.py
+++ snake_game.py
@@ -29,7 +29,7 @@ head.speed(0)
 head.shape("square")
 head.color("black")
 head.penup()
-head.goto(0,0)
+head.goto(0, 0)
 head.direction = "stop"
 
 # Snake food
@@ -98,7 +98,7 @@ while True:
     wn.update()
 
     # Check for a collision with the border
-    if head.xcor() > 290 or head.xcor()<-290 or head.ycor()  >  290 or head.ycor()<-290:
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor()  > 290 or head.ycor() < -290:
         time.sleep(1)
         head.goto(0, 0)
         head.direction = "stop"
@@ -145,7 +145,7 @@ while True:
             high_score = score
         
         pen.clear()
-        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal")) 
+        pen.write("Score: {}  High Score: {}".format(score, high_score), align = "center", font = ("Courier", 24, "normal")) 
 
     # Move the end segments first in reverse order
     for index in range(len(segments)-1, 0, -1):
@@ -157,7 +157,7 @@ while True:
     if len(segments) > 0:
         x = head.xcor()
         y = head.ycor()
-        segments[0].goto(x,y)
+        segments[0].goto(x, y)
 
     move()    
 
